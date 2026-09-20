b3fc6ad6ea758c13ba4bba855a4a7638d26f48b7
bendmetal <39422587+bendmetal@users.noreply.github.com>
2026-09-20 16:03:38 +0300

    Show the result as one line: bold lb, unbolded grey kg (CSS only)

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // EC225 Cat A Helipad Max Landing - Service Worker
 // Caches the app for full offline use after first load
 
-const CACHE_NAME = 'ec225-ca-helipad-v14';
+const CACHE_NAME = 'ec225-ca-helipad-v15';
 const FILES_TO_CACHE = [
   './',
   './index.html'
